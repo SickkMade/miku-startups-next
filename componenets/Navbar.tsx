9d0b9e73da1c45711d7421024cdfaae79171e796
sickk <jaakmyr@gmail.com>
2025-07-06 22:34:03 -0700

    weird error

--- componenets/Navbar.tsx
+++ componenets/Navbar.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { auth } from '@/auth';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
         <nav className="flex justify-between items-center">
-            <Link href="/"/>
-            <Image src="/miku.jpg" alt="logo" width={30} height={30} />
+            <Link href="/"> 
+              <Image src="/miku.jpg" alt="logo" width={30} height={30} />
+            </Link>
+
+            <div className="flex items-center gap-5">
+              {/* {session && session?.user ? {
+
+              }} */}
+            </div>
         </nav>
     </header>
   )
