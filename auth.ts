328457755cc81aa61d5847b70e1e637063385580
sickk <jaakmyr@gmail.com>
2025-07-27 12:53:00 -0700

    added authentication basics

--- auth.ts
+++ auth.ts
@@ -1,3 +1,3 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-export const { auth, handlers } = NextAuth({ providers: [GitHub] })
+export const { auth, handlers, signIn, signOut } = NextAuth({ providers: [GitHub] })
--- componenets/Navbar.tsx
+++ componenets/Navbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { auth } from '@/auth';
+import { auth, signOut, signIn } from '@/auth';
+import { redirect } from 'next/dist/server/api-utils';
 
 const Navbar = async () => {
   const session = await auth();
@@ -13,10 +14,45 @@ const Navbar = async () => {
               <Image src="/miku.jpg" alt="logo" width={30} height={30} />
             </Link>
 
-            <div className="flex items-center gap-5">
-              {/* {session && session?.user ? {
+            <div className="flex items-center gap-5 text-black">
+              {session && session?.user ? (
+                <>
+                <Link href="/startup/create">
+                  <span>Create</span>
+                </Link>
+                
+                <form action={ async () => {
+                  "use server";
 
-              }} */}
+                  await signOut({redirectTo:"/"});
+
+                }}>
+                  <button type="submit" className="cursor-pointer">
+                    <span>Logout</span>
+                  </button>
+                </form>
+                
+
+                <Link href={`/user/${session?.id}`}>
+                  <span>{session.user?.name}</span>
+                </Link>
+                </>
+              ) : (
+                <form action={async () => {
+                  "use server";
+
+                  try {
+                    await signIn("github");
+                  } catch (error) {
+                    console.error("Sign in error:", error);
+                    throw error;
+                  }
+                }}>
+                  <button type="submit" className="cursor-pointer">
+                    Login
+                  </button>
+                </form>
+              )}
             </div>
         </nav>
     </header>
